2fb7be9a3a1315e1ea92f22e1c8757577de39294
matthew <mmbayles@gmail.com>
2018-05-14 10:32:49 -0600

    Added production keys to only work with hs-apps

--- tethysapp/time_series_script_manager/public/js/main.js
+++ tethysapp/time_series_script_manager/public/js/main.js
@@ -1,6 +1,6 @@
 var number = 0;
-var CLIENT_ID = '';
-var API_KEY = '';
+var CLIENT_ID = '289897672263-nba5onfn04b7k36u620i2ipthtnq8hds.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyB96S1nc9Rr0vEVXDsfYE9_hfWOLmMsueA';
 var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
 var SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/drive.file';
 
